8311a3894b40b12c56c50d9c5a8a12d988de9ebf
Danil-Golubev <dokers190rus@mail.ru>
2025-09-25 11:24:26 +0300

    animations fix

--- src/components/Interface/Interface.tsx
+++ src/components/Interface/Interface.tsx
@@ -26,11 +26,10 @@ export const Interface = () => {
     }
   }, []);
 
-
   useEffect(() => {
     const timerId = window.setTimeout(() => {
       setVisuallySelectedItem(selectedItem);
-    }, 20); 
+    }, 20);
 
     return () => {
       clearTimeout(timerId);
@@ -131,82 +130,86 @@ export const Interface = () => {
             transition: 'transform 0.37s cubic-bezier(0.2, 0.9, 0.22, 1)'
           }}>
           {items.map((value, _index) => (
-          <div className="vert-wrapper" key={_index}>
-            <div className="outwrapper">
-              {outMassive.map((outvalue, _outindex) => (
-                <div
-                  key={_outindex}
-                  style={{
-                    marginTop:
-                      _outindex === 0
-                        ? 'calc(var(--item-height) * -1)'
-                        : 'calc(var(--item-height) * -2)',
-                    // backgroundColor: _outindex === outMassive.length - 1 ? 'blue' : 'green',
-                    width: '100%',
-                    height: 'var(--item-height)',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    // backgroundColor: 'green',
-                    boxSizing: 'border-box'
-                  }}
-                  className={_index === selectedItem ? 'outitem out-appear' : 'display'}>
-                  <img
-                    src={outvalue.icon}
-                    alt={outvalue.icon}
-                  />
-                </div>
-              ))}
-            </div>
-            <div
-              className={_index === visuallySelectedItem ? 'item-main selected' : 'item-main'}
-              // style={{ backgroundColor: 'yellow' }}
-            >
-              <div
-                style={{
-                  width: _index === visuallySelectedItem ? '110%' : '100%'
-                  // backgroundColor: 'yellow'
-                }}>
-                <img
-                  alt={value.icon}
-                  src={value.icon}
-                  className={_index === selectedItem ? undefined : 'dimmed'}
-                  style={{
-                    opacity: _index === selectedItem ? 1 : 0.7,
-                    transform: _index === visuallySelectedItem ? 'scale(1.2)' : 'scale(1)',
-                    transition: 'filter 0.31s cubic-bezier(0.2, 0.9, 0.22, 1), opacity 0.31s cubic-bezier(0.2, 0.9, 0.22, 1), transform 0.37s cubic-bezier(0.2, 0.9, 0.22, 1)'
-                  }}
-                />
-                <p
-                  style={{
-                    opacity: _index === selectedItem ? '1' : '0'
-                    // backgroundColor: 'yellow'
-                  }}>
-                  {value.name}
-                </p>
-              </div>
-            </div>
-            <div style={{ display: 'flex', flexDirection: 'column', gap: 'var(--gap-size)', marginTop: 'var(--gap-size)' }}>
-              {_index === selectedItem &&
-                value.semiOptions.map((semivalue: SemiOption, _semiindex: number) => (
+            <div className="vert-wrapper" key={_index}>
+              <div className="outwrapper">
+                {outMassive.map((outvalue, _outindex) => (
                   <div
-                    id={`semi-${_semiindex}`}
-                    className="semi-option"
+                    key={_outindex}
                     style={{
-                      display: _semiindex < outMassive.length ? 'none' : 'flex'
-                      // backgroundColor: 'green'
-                    }}>
-                    <img
-                      alt={semivalue.icon}
-                      src={semivalue.icon}
-                      style={{
-                        opacity: _index === selectedItem ? 1 : 0.7,
-                        borderRightColor: 'yellow'
-                      }}
-                    />
+                      marginTop:
+                        _outindex === 0
+                          ? 'calc(var(--item-height) * -1)'
+                          : 'calc(var(--item-height) * -2)',
+                      // backgroundColor: _outindex === outMassive.length - 1 ? 'blue' : 'green',
+                      width: '100%',
+                      height: 'var(--item-height)',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      // backgroundColor: 'green',
+                      boxSizing: 'border-box'
+                    }}
+                    className={_index === selectedItem ? 'outitem out-appear' : 'display'}>
+                    <img src={outvalue.icon} alt={outvalue.icon} />
                   </div>
                 ))}
+              </div>
+              <div
+                className={_index === visuallySelectedItem ? 'item-main selected' : 'item-main'}
+                // style={{ backgroundColor: 'yellow' }}
+              >
+                <div
+                  style={{
+                    width: _index === visuallySelectedItem ? '110%' : '100%'
+                    // backgroundColor: 'yellow'
+                  }}>
+                  <img
+                    alt={value.icon}
+                    src={value.icon}
+                    className={_index === selectedItem ? undefined : 'dimmed'}
+                    style={{
+                      opacity: _index === selectedItem ? 1 : 0.7,
+                      transform: _index === visuallySelectedItem ? 'scale(1.2)' : 'scale(1)',
+                      transition:
+                        'filter 0.2s cubic-bezier(0.2, 0.9, 0.22, 1), opacity 0.2s cubic-bezier(0.2, 0.9, 0.22, 1), transform 0.2s cubic-bezier(0.2, 0.9, 0.22, 1)'
+                    }}
+                  />
+                  <p
+                    style={{
+                      opacity: _index === selectedItem ? '1' : '0'
+                      // backgroundColor: 'yellow'
+                    }}>
+                    {value.name}
+                  </p>
+                </div>
+              </div>
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  gap: 'var(--gap-size)',
+                  marginTop: 'var(--gap-size)'
+                }}>
+                {_index === selectedItem &&
+                  value.semiOptions.map((semivalue: SemiOption, _semiindex: number) => (
+                    <div
+                      id={`semi-${_semiindex}`}
+                      className="semi-option"
+                      style={{
+                        display: _semiindex < outMassive.length ? 'none' : 'flex'
+                        // backgroundColor: 'green'
+                      }}>
+                      <img
+                        alt={semivalue.icon}
+                        src={semivalue.icon}
+                        style={{
+                          opacity: _index === selectedItem ? 1 : 0.7,
+                          borderRightColor: 'yellow'
+                        }}
+                      />
+                    </div>
+                  ))}
+              </div>
             </div>
-          </div>
           ))}
         </div>
       </div>
